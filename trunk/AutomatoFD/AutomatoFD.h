aa28ab470657a766959cd654cf395861ef1890e5
vibortolotti <vibortolotti@c139fd2d-684a-0410-9d8d-612673a789d6>
2008-04-09 20:52:23 +0000

    Initial import.

---
+++ trunk/AutomatoFD/AutomatoFD.h
@@ -0,0 +1,91 @@
+#ifndef AUTOMATOFD_H_
+#define AUTOMATOFD_H_
+
+#include <iostream>
+#include <list>
+#include <string>
+
+
+
+class AutomatoFD
+{
+	public:
+		AutomatoFD( std::string );
+
+		~AutomatoFD( );
+
+		/*hashTable<tipoDaHash>
+		getHashTable( );*/
+
+	protected:
+		std::list<std::string>
+		codigoPascal;
+
+		std::string
+		token;
+
+		void
+		carregaCodigo( std::string );
+
+		int
+		validaCaractereREGEX( char*, const char*, std::string );
+
+		void
+		adicionaCaractereToken(	);
+
+		void
+		adicionaTokenHash( );
+
+		void
+		estadoS( );
+
+		void
+		estadoA( );
+
+		void
+		estadoB( );
+
+		void
+		estadoC( );
+
+		void
+		estadoD( );
+
+		void
+		estadoE( );
+
+		void
+		estadoF( );
+
+		void
+		estadoG( );
+
+		void
+		estadoH( );
+
+		void
+		estadoI( );
+
+		void
+		estadoJ( );
+
+		void
+		estadoK( );
+
+		void
+		estadoL( );
+		
+		void
+		estadoM( );
+		
+		void
+		estadoN( );
+
+		void
+		estadoO( );
+		
+		void
+		estadoP( );
+};
+
+#endif /*AUTOMATOFD_H_*/
